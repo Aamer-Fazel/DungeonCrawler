7b1239bc8d8bb840dca44d59746e36c196d4de5b
Aamer <bobrickfazel92@gmail.com>
2021-06-30 16:15:29 -0500

    first commit

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+
+    private int maxHealth, currentHealth;
+    private String name;
+    private String weapon;
+    private int maxStamina, currentStamina;
+    private int level;
+    private int strength;
+
+}
